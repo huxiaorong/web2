9d564e2d91047c43f62f9bfd03e043fada72b4e3
huxiaorong <2017704565@qq.com>
2019-01-04 19:21:13 +0800

    message

---
+++ work/shouye.js
@@ -0,0 +1,55 @@
+window.onload=function () {
+	
+	var box11=document.getElementsByClassName('box11')[0];
+	window.onscroll=function(){
+		var st =document.documentElement.scrollTop || document.body.scrollTop;
+		if(st>180){
+			box11.style.position='fixed'
+		}else{
+			box11.style.position='static'
+		}
+	}
+}
+
+function getStyle(obj, attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	} else {
+		return getComputedStyle(obj, null)[attr];
+	}
+}
+function animate(obj,json,callback){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var isStop = true;
+		for(var attr in json){
+			var now = 0;
+			if(attr == 'opacity'){
+				now = parseInt(getStyle(obj,attr)*100);
+			}else{
+				now = parseInt(getStyle(obj,attr));
+			}
+			var speed = (json[attr] - now) / 8;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			var cur = now + speed;
+			if(attr == 'opacity'){
+				obj.style[attr] = cur / 100;
+			}else{
+				obj.style[attr] = cur + 'px';
+			}
+			if(json[attr] !== cur){
+				isStop = false;
+			}
+		}
+		if(isStop){
+			clearInterval(obj.timer);
+			callback&&callback();
+		}
+	}, 30)
+}
+
+
+
+
+
+
---
+++ work/xiangqingye.js
@@ -0,0 +1,72 @@
+var pitch3 = document.getElementById("pitch3");
+var pitch5 = document.getElementById("pitch5");
+var pitch4 = document.getElementById("pitch4");
+pitch3.onclick= function(){
+	console.log("1");
+	if(pitch4.innerHTML==1){
+	    pitch3.setAttribute("style","color: #aca7a2;");
+	    pitch5.setAttribute("style","color: #000;");
+	}
+	else{
+	    var i =pitch4.innerHTML;
+	    i--;
+	    pitch4.innerHTML=i;
+	    pitch3.setAttribute("style","color: #000;");
+	    pitch5.setAttribute("style","color: #000;");
+	    if(pitch4.innerHTML==1){
+	        pitch3.setAttribute("style","color: #aca7a2;");
+	        pitch5.setAttribute("style","color: #000;");
+	    }
+	}
+    
+}
+pitch5.onclick= function(){
+    if(pitch4.innerHTML==5){
+        pitch5.setAttribute("style","color: #aca7a2;");
+        pitch3.setAttribute("style","color: #000;");
+    }
+    else{
+        var i =pitch4.innerHTML;
+        i++;
+        pitch4.innerHTML=i;
+        pitch5.setAttribute("style","color: #000;");
+        pitch3.setAttribute("style","color: #000;");
+        if(pitch4.innerHTML==5){
+            pitch5.setAttribute("style","color: #aca7a2;");
+        }
+    }
+}
+var deal4 = document.getElementById('deal4');
+var deal7 = document.getElementById('deal7');
+console.log(deal4.firstElementChild.nextElementSibling);
+console.log( deal4.lastElementChild);
+deal4.firstElementChild.nextElementSibling.setAttribute("onclick","bit()");
+deal4.lastElementChild.setAttribute("onclick","bit1()");
+function bit(){
+    console.log(1);
+    deal4.firstElementChild.nextElementSibling.setAttribute("id","img1");
+    if(deal4.lastElementChild.hasAttribute("id")){
+        deal4.lastElementChild.removeAttribute("id");
+    }
+    deal7.innerHTML = '"150ml"';
+}
+function bit1(){
+    console.log(2);
+    deal4.lastElementChild.setAttribute("id","img1");
+    if(deal4.firstElementChild.nextElementSibling.hasAttribute("id")){
+        deal4.firstElementChild.nextElementSibling.removeAttribute("id");
+    }
+    deal7.innerHTML = '"200ml"';
+}
+window.onload=function () {
+	
+	var box11=document.getElementsByClassName('box11')[0];
+	window.onscroll=function(){
+		var st =document.documentElement.scrollTop || document.body.scrollTop;
+		if(st>180){
+			box11.style.position='fixed';
+		}else{
+			box11.style.position='static';
+		}
+	}
+}
